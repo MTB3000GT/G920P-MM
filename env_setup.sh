4d78aa38754431db0fb241f21b3b0ba6b22822dc
The-Sickness <alloycowboy6969@gmail.com>
2016-05-08 20:16:36 -0500

    Compile with UBERTC 5.3 Toolchain
    Conflicts:
    	Makefile
    	env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -77,8 +77,7 @@ export TMPFILE=`mktemp -t`;
 
 
 # system compiler
-export CROSS_COMPILE=/home/buildserver/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
-
+export CROSS_COMPILE=/home/buildserver/aarch64-linux-android-5.3-kernel/bin/aarch64-linux-android-
 
 # CPU Core
 export NUMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`;
